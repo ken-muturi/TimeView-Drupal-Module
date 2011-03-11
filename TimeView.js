e2179a5a4a20522d9c52464fc5ff671b558b9f18
Martin Norling <norling.git@gmail.com>
2011-03-11 14:31:09 +0300

    Original Commit

---
+++ TimeView.js
@@ -0,0 +1,26 @@
+// $Id$
+
+var tl;
+
+function TimeView_onLoad(XML_source, target_id) {
+  var eventSource = new Timeline.DefaultEventSource();
+  var bandInfos = [
+    Timeline.createBandInfo({
+      width:          "75%", 
+      eventSource:    eventSource,
+      intervalUnit:   Timeline.DateTime.WEEK, 
+      intervalPixels: 50
+    }),
+    Timeline.createBandInfo({
+      overview:       true,
+      width:          "25%", 
+      eventSource:    eventSource,
+      intervalUnit:   Timeline.DateTime.MONTH, 
+      intervalPixels: 100
+    })
+  ];
+  bandInfos[1].syncWith = 0;
+  bandInfos[1].highlight = true;
+  tl = Timeline.create(document.getElementById(target_id), bandInfos);
+  Timeline.loadXML(XML_source, function(xml, url) { eventSource.loadXML(xml, url); });
+}
