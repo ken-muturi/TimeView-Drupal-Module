ad8e361b683e1d95238111a7f8f68d30840fdb09
Martin Norling <norling.git@gmail.com>
2011-03-16 09:59:59 +0300

    Revert "Fixes a bug in editing timestamps. Adds uploads/ folder."
    This reverts commit e29bd30e8033f1e0ee17ef5c07e25b62227db4e5.

--- TimeView.js
+++ TimeView.js
@@ -10,14 +10,15 @@ function TimeView_onLoad(XML_source, target_id) {
       eventSource:    eventSource,
       intervalUnit:   Timeline.DateTime.WEEK, 
       intervalPixels: 50,
-      timeZone:       3
+      timezone:       3
     }),
     Timeline.createBandInfo({
       overview:       true,
       width:          "25%", 
       eventSource:    eventSource,
       intervalUnit:   Timeline.DateTime.MONTH, 
-      intervalPixels: 100
+      intervalPixels: 100,
+      timezone:       3
     })
   ];
   bandInfos[1].syncWith = 0;
